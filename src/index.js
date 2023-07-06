689658cab318982a0b43656de25f465b86cd2f16
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-07-06 21:23:08 +0300

    final

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import {fetchBreeds, fetchCatByBreed} from "./js/cat-api.js";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    catSelect: document.querySelector(`.breed-select`),
+    textLoader: document.querySelector(`.loader`),
+    catInfo: document.querySelector(`.cat-info`),
+}
+
+
+
+startLoad();
+refs.catSelect.addEventListener(`change`, getSelectCat);
+
+
+
+
+
+
+
+
+
+function startLoad() {
+    refs.textLoader.hidden = false;
+    fetchBreeds().then(renderSelectCat).catch(handError);
+}
+
+function getSelectCat(e) {
+    refs.catInfo.innerHTML = ``;
+    refs.textLoader.hidden = false;
+    const idSelectCat = e.currentTarget.value;
+    fetchCatByBreed(idSelectCat).then(renderCat).catch(handError);
+    
+}
+
+
+function renderSelectCat(data) {
+        refs.textLoader.hidden = true;
+        refs.catSelect.innerHTML = getMarkBreeds(data);
+        refs.catSelect.hidden = false;   
+}
+
+function renderCat(data) {
+        refs.catInfo.innerHTML = getMarkCat(data);
+        refs.textLoader.hidden = true;
+}
+
+function getMarkCat(data) {
+    const catImg = data.url;
+    const cat = data.breeds[0];
+    return `<div class="flex-item"><img src="${catImg}" width=500 class = "cat-img"></img></div>
+    <div class="flex-item"><h1 class = "cat-title">${cat.name}</h1>
+    <p>${cat.description}</p>
+    <p><b>Temperament:</b> ${cat.temperament}</p></div>`;}
+   
+
+function getMarkBreeds(data) {
+   return data.map(cat => {
+    return `<option value="${cat.id}">${cat.name}</option>`
+   }).join(``);
+}
+
+function handError() {
+    refs.textLoader.hidden = true;
+    Notify.failure("На жаль, сталася помилка");
+}
+
+
+
+
+
---
+++ src/js/cat-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_CUAb11DLcFpMAp9svBo6Rj5evX19DFM5pBvuGTp2eKqkLjhuZNvEtiScp5c61zi1";
+
+function fetchBreeds() {
+    return axios.get(`https://api.thecatapi.com/v1/breeds`).then(data => {
+        return data.data});
+}
+
+function fetchCatByBreed(id) {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`).then(data => {
+        return data.data[0];
+        // return data.data[0].breeds[0];
+    });
+}
+
+
+export {fetchBreeds, fetchCatByBreed};
